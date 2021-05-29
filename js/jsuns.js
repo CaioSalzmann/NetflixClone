bf8898b774c874ccc8ec145509715717205c74a0
CaioSalzmann <caio.cmgm@gmail.com>
2021-05-29 09:46:55 -0300

    Netflixclone

---
+++ js/jsuns.js
@@ -0,0 +1,7 @@
+function trocar(elemento){
+    elemento.innerHTML = "+41 077 470 51 73";
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Phone";
+    }
---
+++ js/teste.js
@@ -0,0 +1,80 @@
+jQuery(function($) {
+    $('#golink1').click(function() {
+        return false;
+    }).dblclick(function() {
+        window.location = this.href;
+        return false;
+    });
+});
+
+jQuery(function($) {
+    $('#golink2').click(function() {
+        return false;
+    }).dblclick(function() {
+        window.location = this.href;
+        return true;
+    });
+});
+
+jQuery(function($) {
+    $('#golink3').click(function() {
+        return false;
+    }).dblclick(function() {
+        window.location = this.href;
+        return true;
+    });
+});
+
+jQuery(function($) {
+    $('#golink4').click(function() {
+        return false;
+    }).dblclick(function() {
+        window.location = this.href;
+        return false;
+    });
+});
+
+jQuery(function($) {
+    $('#golink5').click(function() {
+        return false;
+    }).dblclick(function() {
+        window.location = this.href;
+        return true;
+    });
+});
+
+jQuery(function($) {
+    $('#golink6').click(function() {
+        return false;
+    }).dblclick(function() {
+        window.location = this.href;
+        return true;
+    });
+});
+
+jQuery(function($) {
+    $('#golink7').click(function() {
+        return false;
+    }).dblclick(function() {
+        window.location = this.href;
+        return true;
+    });
+});
+
+jQuery(function($) {
+    $('#golink8').click(function() {
+        return false;
+    }).dblclick(function() {
+        window.location = this.href;
+        return true;
+    });
+});
+
+jQuery(function($) {
+    $('#golink9').click(function() {
+        return false;
+    }).dblclick(function() {
+        window.location = this.href;
+        return true;
+    });
+});
